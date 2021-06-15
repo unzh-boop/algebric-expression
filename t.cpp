e7a1a5d30d9a2acda5e931331afb3297f2efbe2d
unzh-boop <uneezehhabib@gmail.com>
2021-06-16 01:08:46 +0500

    commit

---
+++ t.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, k;
+    float y;
+
+    cout << "Enter x and k ";
+    cin >> x >> k;
+
+
+    y = ((3*x)-(5*k)) * (x/k);
+
+
+    cout << y;
+
+    return 0;
+}
